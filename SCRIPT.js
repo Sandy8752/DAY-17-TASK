844631f330f790341cdbd70b0d1703ded5f1ac35
SANTHOSH KUMAR K <93277097+Sandy8752@users.noreply.github.com>
2022-09-30 16:08:51 +0530

    Add files via upload

---
+++ SCRIPT.js
@@ -0,0 +1,79 @@
+let count1 = 11;
+setTimeout(() => {
+    count1--;
+    console.log(count1);
+    var target=document.getElementById('test');
+    target.innerText=count1;
+    setTimeout(() => {
+        count1--;
+        console.log(count1);
+        var target=document.getElementById('test');
+        target.innerText=count1;
+        setTimeout(() => {
+            count1--;
+            console.log(count1);
+            var target=document.getElementById('test');
+            target.innerText=count1;
+            setTimeout(() => {
+                count1--;
+                console.log(count1);
+                var target=document.getElementById('test');
+                target.innerText=count1;
+                setTimeout(() => {
+                    count1--;
+                    console.log(count1);
+                    var target=document.getElementById('test');
+                    target.innerText=count1;
+                    setTimeout(() => {
+                        count1--;
+                        console.log(count1);
+                        var target=document.getElementById('test');
+                        target.innerText=count1;
+                        setTimeout(() => {
+                            count1--;
+                            console.log(count1);
+                            var target=document.getElementById('test');
+                            target.innerText=count1;
+                            setTimeout(() => {
+                                count1--;
+                                console.log(count1);
+                                var target=document.getElementById('test');
+                                target.innerText=count1;
+                                setTimeout(() => {
+                                    count1--;
+                                    console.log(count1);
+                                    var target=document.getElementById('test');
+                                    target.innerText=count1;
+                                    setTimeout(() => {
+                                        count1--;
+                                        console.log(count1);
+                                        var target=document.getElementById('test');
+                                        target.innerText=count1;
+                                        setTimeout(() => {
+                                            count1--;
+                                            console.log(count1);
+                                            var target=document.getElementById('test');
+                                            target.innerText=count1;
+                                            clearTimeout();
+                                                console.log("SANTHOSH KUMAR ");
+                                                var target=document.getElementById('test');
+                                                target.innerText="SANTHOSH KUMAR ";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
+
+
+
+
+
+
+
+
